79d7b8ace2825519051b13bf158cd2bf56a2b481
Jannis Adamek <jannis.adamek@gmail.com>
2022-12-30 18:13:00 +0100

    Add basic project structure with Vite and Vue.

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Memory from './components/Memory.vue';
+import CodeEditor from './components/CodeEditor.vue';
+</script>
+
+<template>
+  <Memory />
+  <CodeEditor />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Memory.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const memory = ref(new Int16Array(10));
+
+function padded(number: number, base: number = 2, padding: number = 8): string {
+    return number.toString(base).padStart(padding, '0');
+}
+
+</script>
+
+<template>
+    <h1>Memory</h1>
+    <table>
+        <tr>
+            <th>Address</th>
+            <th>Value</th>
+            <th>Edit</th>
+        </tr>
+        <tr v-for="(number, index) in memory">
+            <td>{{ padded(index, 2) }}</td>
+            <td>{{ padded(number, 2) }}</td>
+            <td><button>Edit</button></td>
+        </tr>
+    </table>
+</template>
+
+<style scoped>
+table,
+th,
+td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+</style>
