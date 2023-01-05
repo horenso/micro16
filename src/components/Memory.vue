62f4db98118d9b2abd82f4ce171e0dcb94f335d4
Jannis Adamek <jannis.adamek@gmail.com>
2023-01-05 20:49:19 +0100

    wip

--- src/components/Memory.vue
+++ src/components/Memory.vue
@@ -2,24 +2,31 @@
 import { ref } from 'vue';
 import { padded } from '../service/formatting';
 
-const memory = ref(new Int16Array(10));
+const TABLE_SIZE = 30;
+const offset = ref(0);
+const memory = ref(new Int16Array(20));
 
 </script>
 
 <template>
     <h1>Memory</h1>
-    <table>
-        <tr>
-            <th>Address</th>
-            <th>Value</th>
-            <th>Edit</th>
-        </tr>
-        <tr v-for="(number, index) in memory">
-            <td>{{ padded(index, 2) }}</td>
-            <td>{{ padded(number, 2) }}</td>
-            <td><button>Edit</button></td>
-        </tr>
-    </table>
+    Memory from state: <input type="number" v-model="offset" />
+    {{ offset }}
+    <div class="wrapper">
+        <table>
+            <tr>
+                <th>Address</th>
+                <th>Value</th>
+                <th>Edit</th>
+            </tr>
+            <tr v-for="(number, index) in memory">
+                <td>{{ padded(index, 2) }}</td>
+                <td>{{ padded(number, 2) }}</td>
+                <td><button>Edit</button></td>
+            </tr>
+        </table>
+    </div>
+
 </template>
 
 <style scoped>
@@ -28,5 +35,11 @@ th,
 td {
     border: 1px solid black;
     border-collapse: collapse;
+}
+
+.wrapper {
+    height: 300px;
+    width: 400px;
+    overflow: scroll;
 }
 </style>
