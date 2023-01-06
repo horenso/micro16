2049c7151e6001103269e03d22261e83fa3bcc03
Jannis Adamek <jannis.adamek@gmail.com>
2023-01-06 15:42:19 +0100

    Display registers by name.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import Toolbar from "./components/Toolbar.vue";
 import Memory from "./components/Memory.vue";
+import Registers from "./components/Registers.vue";
 import CodeEditor from "./components/CodeEditor.vue";
 </script>
 
 <template>
   <Toolbar />
   <Memory />
+  <Registers />
   <CodeEditor />
 </template>
 
--- src/components/Registers.vue
+++ src/components/Registers.vue
@@ -1,31 +1,47 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
+import { padded } from "../service/formatting";
 
-const registers = ref(new Int16Array(16));
+const REGISTER_NAMES = [
+  "PC",
+  "R0",
+  "R1",
+  "R2",
+  "R3",
+  "R4",
+  "R5",
+  "R6",
+  "R7",
+  "R8",
+  "R9",
+  "R10",
+  "AC",
+];
+const REGISTER_COUNT = REGISTER_NAMES.length;
 
+const registers = ref(new Int16Array(REGISTER_COUNT));
 </script>
 
 <template>
-    <h1>Memory</h1>
-    <table>
-        <tr>
-            <th>Address</th>
-            <th>Value</th>
-            <th>Edit</th>
-        </tr>
-        <tr v-for="(number, index) in memory">
-            <td>{{ padded(index, 2) }}</td>
-            <td>{{ padded(number, 2) }}</td>
-            <td><button>Edit</button></td>
-        </tr>
-    </table>
+  <h1>Registers</h1>
+  <table>
+    <tr>
+      <th>Register</th>
+      <th>Value</th>
+    </tr>
+    <tr v-for="(number, index) in registers">
+      <td>{{ REGISTER_NAMES[index] }}</td>
+      <td>{{ padded(number, 2, 16) }}</td>
+    </tr>
+  </table>
 </template>
 
 <style scoped>
 table,
 th,
 td {
-    border: 1px solid black;
-    border-collapse: collapse;
+  border: 1px solid black;
+  border-collapse: collapse;
+  font-family: "Courier New", monospace;
 }
 </style>
