cd4079a3a1a8908c9d09d668c60a7668f77e90ec
Jannis Adamek <jannis.adamek@gmail.com>
2023-01-06 15:17:44 +0100

    Add Toolbar component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import Memory from './components/Memory.vue';
-import CodeEditor from './components/CodeEditor.vue';
+import Toolbar from "./components/Toolbar.vue";
+import Memory from "./components/Memory.vue";
+import CodeEditor from "./components/CodeEditor.vue";
 </script>
 
 <template>
+  <Toolbar />
   <Memory />
   <CodeEditor />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from "vue";
+</script>
+
+<template>
+  <button>Reset</button>
+  Clock speed: <input /> Hz
+</template>
+
+<style scoped></style>
