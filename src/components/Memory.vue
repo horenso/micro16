ff0e4f7ecc7f798081e8dfe6f62845c478a21d16
Jannis Adamek <jannis.adamek@gmail.com>
2023-01-06 15:18:28 +0100

    Implement basic memory view.

--- src/components/Memory.vue
+++ src/components/Memory.vue
@@ -1,45 +1,66 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { padded } from '../service/formatting';
+import { ref, onBeforeMount } from "vue";
+import { padded } from "../service/formatting";
 
-const TABLE_SIZE = 30;
+const CAPACITY = 2 ** 16;
+const TABLE_SIZE = 10;
+const MAX_OFFSET = CAPACITY - TABLE_SIZE;
+
 const offset = ref(0);
-const memory = ref(new Int16Array(20));
+const memory = ref(new Int16Array(CAPACITY));
 
+onBeforeMount(() => {
+  for (let i = 0; i < CAPACITY; i++) {
+    memory.value[i] = Math.floor(Math.random() * 10000);
+  }
+});
+
+function updateOffset(event) {
+  const newValue = parseInt(event.target.value);
+  if (isNaN(newValue)) {
+    offset.value = 0;
+  } else if (newValue < 0) {
+    offset.value = 0;
+  } else if (newValue > MAX_OFFSET) {
+    offset.value = MAX_OFFSET;
+  } else {
+    offset.value = newValue;
+  }
+}
 </script>
 
 <template>
-    <h1>Memory</h1>
-    Memory from state: <input type="number" v-model="offset" />
-    {{ offset }}
-    <div class="wrapper">
-        <table>
-            <tr>
-                <th>Address</th>
-                <th>Value</th>
-                <th>Edit</th>
-            </tr>
-            <tr v-for="(number, index) in memory">
-                <td>{{ padded(index, 2) }}</td>
-                <td>{{ padded(number, 2) }}</td>
-                <td><button>Edit</button></td>
-            </tr>
-        </table>
-    </div>
-
+  <h1>Memory</h1>
+  Memory from state:
+  <input type="number" v-model="offset" @input="updateOffset" min="0" :max="MAX_OFFSET" />
+  <div class="wrapper">
+    <table>
+      <tr>
+        <th>Address</th>
+        <th>Value</th>
+        <th>Edit</th>
+      </tr>
+      <tr v-for="(_, i) in TABLE_SIZE">
+        <td>{{ padded(i + offset, 2, 16) }}</td>
+        <td>{{ padded(memory[i + offset], 2, 16) }}</td>
+        <td><button>Edit</button></td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <style scoped>
 table,
 th,
 td {
-    border: 1px solid black;
-    border-collapse: collapse;
+  border: 1px solid black;
+  border-collapse: collapse;
+  font-family: "Courier New", monospace;
 }
 
 .wrapper {
-    height: 300px;
-    width: 400px;
-    overflow: scroll;
+  height: 300px;
+  width: 400px;
+  overflow: scroll;
 }
 </style>
--- src/service/formatting.ts
+++ src/service/formatting.ts
@@ -3,5 +3,9 @@ export function padded(
     base: number = 2,
     padding: number = 8
 ): string {
+    if (number === undefined) {
+        return "";
+    }
+    console.log(number);
     return number.toString(base).padStart(padding, '0');
 }
