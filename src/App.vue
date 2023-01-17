d71e0ebb7a08456a9807998a04c5d6b14b595d1c
Jannis Adamek <jadamek@lithoz.com>
2023-01-17 16:26:07 +0100

    svg component test

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@ import Toolbar from "./components/Toolbar.vue";
 import Memory from "./components/Memory.vue";
 import Registers from "./components/Registers.vue";
 import CodeEditor from "./components/CodeEditor.vue";
+import Graph from "./components/Graph.vue";
 </script>
 
 <template>
-    <Toolbar />
-    <CodeEditor />
+    <!-- <Toolbar /> -->
+    <Graph />
+    <!-- <CodeEditor />
     <Memory />
-    <Registers />
+    <Registers /> -->
 </template>
 
 <style scoped>
---
+++ src/components/Graph.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+const box1Visable = ref(true);
+const box2Visable = ref(true);
+
+function boolToVisableHidden(visable: boolean): string {
+    return visable ? 'visable' : 'hidden';
+}
+
+function buttonName(visable: boolean): string {
+    return visable ? 'Hide' : 'Show';
+}
+
+</script>
+
+<template>
+    <button @click="box1Visable = !box1Visable">{{ buttonName(box1Visable) }}</button>
+    <button @click="box2Visable = !box2Visable">{{ buttonName(box2Visable) }}</button>
+
+    <p>
+        <svg width="200px" height="200px" version="1.1" id="svg5" sodipodi:docname="test.svg">
+            <rect id="rect1" width="70.0" height="60.0" x="25.0" y="80.0"
+                :visibility="boolToVisableHidden(box1Visable)" />
+            <rect id="rect2" width="70.0" height="60.0" x="70.0" y="125.0"
+                :visibility="boolToVisableHidden(box2Visable)" />
+        </svg>
+    </p>
+</template>
+
+<style scoped>
+svg {
+    border: 1px solid black;
+}
+
+#rect1 {
+    fill: #0000ff;
+}
+
+#rect2 {
+    fill: #00ff00;
+}
+</style>
