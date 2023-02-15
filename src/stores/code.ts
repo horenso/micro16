960762d20a5702c0af11f56d04b2f2790fb5f80d
Jannis Adamek <jannis.adamek@gmail.com>
2023-02-15 13:13:04 +0100

    Modify code store to use the new assembly result structure.

--- src/stores/code.ts
+++ src/stores/code.ts
@@ -5,31 +5,50 @@ import { useSettingsStore } from './settings';
 
 export const useCodeStore = defineStore('code', {
     state: () => ({
-        code: "",
+        code: '',
         isDirty: true,
-        assembledCode: []
-    }), getters: {
+        assembledCode: [],
+    }),
+    getters: {
         assembledCodeString: (state) => {
-            return state.assembledCode.map(code => formatNumber(code, useSettingsStore().numberSystem, 32)).join('\n');
-        }
-    }, actions: {
+            return state.assembledCode
+                .map((code) => {
+                    if (typeof code === 'string') {
+                        return code;
+                    } else {
+                        return formatNumber(
+                            code,
+                            useSettingsStore().numberSystem,
+                            32
+                        );
+                    }
+                })
+                .join('\n');
+        },
+    },
+    actions: {
         assemble() {
-            if (this.code === "") {
-                this.assembledCode = []
+            if (this.code === '') {
+                this.assembledCode = [];
                 return;
             }
             let newAssembledCode: any = [];
             const lines = this.code.split('\n');
             // Remove the last empty line
             // "a\nb\n" should become ["a","b"] not [a,b,""]
-            if (lines[lines.length-1] == '') {
+            if (lines[lines.length - 1] == '') {
                 lines.pop();
             }
-            lines.forEach(line => {
-                newAssembledCode.push(assembleLine(line));
+            lines.forEach((line) => {
+                const result = assembleLine(line);
+                if (result.ok) {
+                    newAssembledCode.push(result.result);
+                } else {
+                    newAssembledCode.push(result.error.message);
+                }
             });
             this.assembledCode = newAssembledCode;
             console.log(this.assembledCode);
         },
-    }
+    },
 });
