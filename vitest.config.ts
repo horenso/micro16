f4f5e71507a71994515cee246ba22ff727448449
Jannis Adamek <jannis.adamek@gmail.com>
2023-03-12 15:22:45 +0100

    Add resolve '@' to vitest config.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,12 @@
-import { configDefaults, defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
     test: {
         include: ['**/test.*.ts'],
     },
